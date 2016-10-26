ed35c95843757335171c4a0621c5517349a6f149
Jefferson-Faseler <jjfaseler@gmail.com>
2016-10-26 14:04:30 -0400

    Fix employees to be integer. Add until loop.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,19 +13,24 @@ end
 until num_emp == 0
   puts "#{num_emp} quizzes remaining"
   puts "What is your name?"
-  name = gets.chomp
+    name = gets.chomp
   puts "How old are you?"
-  age = gets.chomp.to_i
+    age = gets.chomp.to_i
   puts "What year were you born?"
-  dob = gets.chomp.to_i
+    dob = gets.chomp.to_i
   puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-  bread = gets.chomp
+    bread = gets.chomp
   puts "Would you like to enroll in the company's health insurance?"
-  insurance = gets.chomp
+    insurance = gets.chomp
+  
+  until allergies == "done" || allergies == "sunshine"
+    puts "Do you have any allergies?"
+    allergies = gets.chomp
+  end
 
   if age_verifier( age, dob) == true && (bread == "yes" || insurance == "yes")
     puts "Probably not a vampire"
-  elsif age_verifier( age, dob) == false && (bread == "no" || insurance == "no")
+  elsif age_verifier( age, dob) == false && (bread == "no" || insurance == "no") || allergies == "sunshine"
     puts "Probably a vampire"
   elsif age_verifier( age, dob) == false && bread == "no" && insurance == "no"
     puts "Almost certainly a vampire"
