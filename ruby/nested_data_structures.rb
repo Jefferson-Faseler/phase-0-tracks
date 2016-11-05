4ec6932ef27364c3bc82491ffd5d4afe7774bdfc
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-05 09:28:25 -0400

    Add deep nesting access.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -5,14 +5,14 @@ bookshelf = {
     books: [
       "HTML & CSS by Jon Duckett",
       "Javascript & JQuery by Jon Duckett",
-      "journal 5"
+      "Journal 5"
     ],
     electronics: [
       "Children's Music CD's",
       "Leapfrog Tablet",
       "Singing Barbie doll (she can't see it this high up)"
     ],
-    ollectibles: [
+    collectibles: [
       "Texas Rangers snapback cap",
       "Daughter's painting",
       "Boba Fett rare all-white concept armor action figure",
@@ -21,12 +21,12 @@ bookshelf = {
   },
   upper_left: {
     books: [
-      "journal 1",
-      "journal 2",
-      "journal 3",
+      "Journal 1",
+      "Journal 2",
+      "Journal 3",
       "Star Wars: Tales of the Bounty Hunters",
       "Sacred Marriage",
-      "journal 4",
+      "Journal 4",
       "Azimov's Caliban",
       "Mary Oliver's Poetry Handbook",
       "All Creatures Great and Small"
@@ -39,7 +39,7 @@ bookshelf = {
       "Headlamp",
       "Flashlight"
     ],
-    art_supplies: [
+    art_supplies: {
       assorted_brush_pens: {
         s_brush: "black",
         m_brush: {
@@ -55,8 +55,8 @@ bookshelf = {
         xb_brush: "black"
       },
       drawing_pad: "small",
-      drawing_journal: "small drawing journal"
-    ],
+      drawing_journal: "Small drawing journal"
+    },
     collectibles: [
       "Beard comb inscribed 'Daddy's Whiskers'",
       "Boba Fett action figure",
@@ -142,7 +142,7 @@ bookshelf = {
       "On The Night You Were Born",
       "Thank God for Puppies!",
       "The Cat Book",
-      "Where the WIld Things Are",
+      "Where the Wild Things Are",
       "Press Here",
       "Dada",
       "Star Wars Storybook Treasury",
@@ -155,8 +155,21 @@ bookshelf = {
 puts "Some of my favorite things in the world are on this book shelf."
 puts "Some hobbies, some memories, some for fun, some have no emotional value at all."
 puts "But I think this bookshelf is an expression of my life in some ways."
-puts "For instance there are seven journals total of mine on this bookshelf."
-puts "#{bookshelf[:upper_left][:books][0]} #{bookshelf[:upper_left][:books][1]} 
-#{bookshelf[:upper_left][:books][2]} #{bookshelf[:upper_left][:books][5]}
-#{bookshelf[:top][:books][2]} #{bookshelf[:upper_left][:art_supplies][:drawing_journal]} 
-#{bookshelf[:mid_right][:books][1]}"
+puts "For instance there are seven journals on this bookshelf."
+puts bookshelf[:upper_left][:books][0], bookshelf[:upper_left][:books][1], 
+bookshelf[:upper_left][:books][2], bookshelf[:upper_left][:books][5], 
+bookshelf[:top][:books][2], bookshelf[:upper_left][:art_supplies][:drawing_journal], 
+bookshelf[:mid_right][:books][1]
+
+puts "I've also got a few Star Wars collectibles I'm very proud of"
+puts bookshelf[:top][:collectibles][2], bookshelf[:upper_left][:books][3], bookshelf[:upper_left][:collectibles][1]
+puts "Actually, come to think of it, I missed one!"
+bookshelf[:upper_left][:collectibles].push("Star Wars Rebel insignia belt buckle")
+puts bookshelf[:upper_left][:collectibles][5]
+
+puts "My daughter is very major part of my life, I'm a stay-at-home dad so we spend a lot of time together."
+puts "Today she decided it was time to take all the shoes out of the shelf"
+annas_play_time = bookshelf[:bottom_left][:childrens_footwear]
+bookshelf[:bottom_left].delete(:childrens_footwear)
+p bookshelf[:bottom_left]
+puts "#{annas_play_time} is now scattered across the house... gotta love her still"
