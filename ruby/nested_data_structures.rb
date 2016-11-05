aa3fc8551c3e2b1ee07c7ec1961a2589f8ff4da5
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-04 23:11:14 -0400

    Add nested data of items in my bookshelf

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,150 @@
+## ITEMS IN MY BOOKSHELF ##
+
+bookshelf = { 
+  top: = {
+    books: = [
+      "HTML & CSS by Jon Duckett",
+      "Javascript & JQuery by Jon Duckett",
+      "journal 5"
+    ],
+    electronics: = [
+      "Children's Music CD's",
+      "Leapfrog Tablet",
+      "Singing Barbie doll (she can't see it this high up)"
+    ],
+    ollectibles: = [
+      "Texas Rangers snapback cap",
+      "Daughter's painting",
+      "Boba Fett rare all-white concept armor action figure",
+      "Dad's pocket knife"
+    ],
+  },
+  upper_left: = {
+    books: = [
+      "journal 1",
+      "journal 2",
+      "journal 3",
+      "Star Wars: Tales of the Bounty Hunters",
+      "Sacred Marriage",
+      "journal 4",
+      "Azimov's Caliban",
+      "Mary Oliver's Poetry Handbook",
+      "All Creatures Great and Small"
+    ],
+    electronics: = [
+      "USB charger",
+      "Headphones",
+      "Phone cords",
+      "Charging blocks",
+      "Headlamp",
+      "Flashlight"
+    ],
+    art_supplies: = [
+      assorted_brush_pens: = {
+        s_brush: "black",
+        m_brush: = {
+          black: "black",
+          blue: "blue",
+          sage: "sage",
+          violet: "violet",
+          red: "red",
+          marigold: "marigold",
+          lemon_yellow: "lemon_yellow"
+        },
+        b_brush: "black",
+        xb_brush: "black"
+      },
+      drawing_pad: "small",
+      drawing_journal: "small drawing journal"
+    ],
+    collectibles: = [
+      "Beard comb inscribed 'Daddy's Whiskers'",
+      "Boba Fett action figure",
+      "Harry Potter Platform 9 3/4 ticket",
+      "Magic Cards (if I hash this it would never end)",
+      "Picture of Dad"
+    ],
+  },  
+  upper_right: = {
+    hygiene: = [
+      "Deodorant",
+      "Lotion",
+      "Floss",
+      "Comb",
+      "Wife's makeup", #I shouldn't feel like I need to say that. It's 2016, man
+      "Nail polish",
+      "Some weird lotion I can't explain",
+    ],
+    "Texas Rangers coffee mug"
+  },
+  mid_left: = {
+    file_folders: = {
+      personal_records: = [
+        "Letters",
+        "Identification",
+        "sticker sheet",
+      ],
+      paid_bills: = [
+        "ConEd",
+        "National Grid",
+        "Optimum",
+        "Student Loans",
+        "Doctor",
+        "Life Insurance",
+        "Credit Cards"
+      ],
+      to_be_completed: = [
+        "Daughter's day-care papers",
+        "MTA Incident Report",
+        "Parking Ticket",
+        "Bills"
+      ],
+    },
+  },
+  mid_right: = {
+    electronics: = [
+      "Power strip",
+      "Heating pad",
+      "VR Headset",
+      "Kindle"
+    ],
+    books: = [
+      "Bible",
+      "Journal 6"
+    ],
+  },
+  bottom_left: = {
+    childrens_footwear: = [
+      "Cat shoes",
+      "Minnie Mouse shoes",
+      "Dorothy Red Slippers",
+      "Swimming sandals",
+      "flip flops",
+      "White and purple flower shoes",
+      "Black shoes",
+      "Purple and white athletic shoes",
+      "Aqua and orange Nikes",
+      "Snow boots",
+      "Frozen rain boots"
+    ],
+  },
+  bottom_right: = {
+    childrens_books = [
+      "Peanut Butter and Cupcake",
+      "The Giving Tree",
+      "Alexander's Terrible, No Good, Very Bad Day",
+      "Darth Vader and Son",
+      "Darth Vader and Princess",
+      "First Words",
+      "Sammy the Seal",
+      "On The Night You Were Born",
+      "Thank God for Puppies!",
+      "The Cat Book",
+      "Where the WIld Things Are",
+      "Press Here",
+      "Dada",
+      "Star Wars Storybook Treasury",
+      "Shel Silverstein Treasury"
+    ],
+  },
+},
