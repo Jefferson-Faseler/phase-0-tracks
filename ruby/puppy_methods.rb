ee4b681e06dd652140f4c10159b12b99aacd0cdc
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-08 13:33:54 -0500

    Add class methods and driver code.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,36 @@ class Puppy
     toy
   end
 
+  def speak(bark)
+    bark.times {|x| puts "woof"}
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(years)
+    age = years * 7
+    puts "The dog is #{age} in dog years."
+  end
+
+  def shake
+    puts "*The dog shakes your hand*"
+  end
+
 end
+
+# ## DRIVER CODE
+
+puppy = Puppy.new
+puppy.fetch("ball")
+## ==> I brought back the ball!
+puppy.speak(5)
+## ==> working
+puppy.roll_over
+## ==> *rolls over*
+puppy.dog_years(3)
+## ==> the dog is 21 years old!
+puppy.shake
+## ==> *shakes hand*
 
