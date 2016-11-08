864679d3a85b9bf2a359615fa30905cc853eb6f2
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-07 20:46:24 -0500

    Add user input for Santa info.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,12 @@
 class Santa
   
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance."
+    @gender = gender
+    @ethnicity = ethnicity
+    @age = 0
+  end
+  
   def speak
     puts "Ho, ho, ho! Happy holidays!"
   end
@@ -8,17 +15,35 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 
-  def initialize
-    puts "Initializing Santa instance."
-  end
+  reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", 
+    "Comet", "Cupid", "Donner", "Blitzen"]
 
 end
 
-## DRIVER CODE ##
-beardy = Santa.new.speak
-# ==> Initializing Santa instance.
-# ==> Ho, ho, ho! Happy holidays!
-fatherxmas = Santa.new.eat_milk_and_cookies("double chocolate")
-# ==> Initializing Santa instance.
-# ==> That was a good double chocolate!
+# ## DRIVER CODE ##
+# beardy = Santa.new.speak
+# # ==> Initializing Santa instance.
+# # ==> Ho, ho, ho! Happy holidays!
+# fatherxmas = Santa.new.eat_milk_and_cookies("double chocolate")
+# # ==> Initializing Santa instance.
+# # ==> That was a good double chocolate!
+
+santas = []
+santa_genders = []
+santa_ethnicities = []
+
+puts "How many Santas are signing up?"
+num_of_santas = gets.chomp.to_i
+
+num_of_santas.times do
+  puts "Hello, Santa. Tell me about yourself."
+  puts "gender: "
+  santa_genders << gets.chomp
+  puts "ethnicity: "
+  santa_ethnicities << gets.chomp
+end
+
+num_of_santas.times do |i|
+  santas << Santa.new(santa_genders[i], santa_ethnicities[i])
+end
 
