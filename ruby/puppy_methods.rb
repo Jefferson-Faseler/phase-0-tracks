637d560f8f17f031ac1e218df98cfcce8063c8eb
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-08 13:58:05 -0500

    Add Books class with methods. Add testing loop to create 50 books.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -38,3 +42,44 @@ puppy.dog_years(3)
 puppy.shake
 ## ==> *shakes hand*
 
+class Books
+
+  def initialize
+    puts "---------------------------"
+    puts "Initializing instance of books"
+  end
+
+  def comic_books
+    puts "Comic books. Kapow!"
+  end
+
+  def novel
+    puts "Novels are so romantic..."
+  end
+
+  def textbook
+    puts "Textbooks are boooooooooooooooring."
+  end
+
+  def childrens_book
+    puts "Cow goes moo."
+  end
+
+end
+
+books = Books.new
+stored_books = []
+
+while stored_books.length <= 50
+  book = Books.new
+  stored_books << book
+end
+
+stored_books.each do |books|
+  books.comic_books
+  books.novel
+  books.textbook
+  books.childrens_book
+end
+
+p stored_books
