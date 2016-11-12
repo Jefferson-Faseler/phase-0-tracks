b904929437afe37914140edf21298f1810f6e5f3
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-11 22:09:11 -0500

    Reintialize commit for hangman game.

--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -1,50 +1,52 @@
-# hangman should have user enter word.
-# new user enters a letter to guess that word.
-# that letter is compared to the word by index.
-# displays the letter and its place if it is in the word.
-# if not it adds a strike out of amount. 
-#   strike maximums are determined by value each unique character
-#   and by the length of the word
-#     1.5 strikes per uniq char
-#               +
-#     1/2 point per letter length
-#               = 
-#     strike max
-# if a letter has alrady been guessed 
-# it does not count against the player.
-# prints the resluts after each guess 
-#   underscores for unguessed letter 
-#   and the letter for correct guesses.
+# Hangman.
 
-require_relative 'hangman'
+# Player 1 picks a word. Player 2 must choose from
+# the alphabet different letters to make up the
+# word.
 
-describe Hangman do
-let(:hangman) { Hangman.new }
+# If user 2 cannot figure out the word before
+# the number of guesses reaches 0 then player 2 
+# loses and player 1 wins. 
 
-  it "creates array of word" do
-    expect(hangman.word_to_arr('abc')).to match_array(['a','b','c'])
-  end
+# define methods for:
+# user to enter word
 
-  it "recieves letter" do
-    expect(hangman.letter_guess('b')).to eq('b')
-  end 
+# transform word into list with values
+# steps: 
+#   split string into hash with letters as keys
+#   set default values as false to indicate not guessed
 
-  it "compares letter to word" do
-    expect(hangman.compare(['a','b','c'],'b')).to eq(1)
-  end
+# another user to enter letters for guess
+# input letter as argument
+# steps: 
+#   compare to guess-hash
 
-  it "morphs array into '_'" do
-    expect(hangman.morph_arr(['a','b','c'])).to contain_exactly('_','_','_')
-  end
+# compare guessed letter to guess-hash
+# input instance variable
+# steps: 
+#   compare letter to each letter in guess-hash
+#   if letter is equal to index change value to true
 
-  it "displays letter in word if guessed correctly" do
-    expect(hangman.display('b', 1)).to contain_exactly('_ b _')
-  end
+# compare the guessed letter to the word
+# steps: 
+#   compare letter to each letter in word-hash
+#   if letter is equal to index change value to true
 
-  it "adds strike to maximum strike count" do
-  end
+# display a printed version of the 
+# correctly guessed letters with underscores 
+# for letters that still need to be discovered
+# steps: 
+#   Print hash symbols for all values in word-hash that 
+#   evaluate to true. 
+#   If they evaluate to false print an underscore
 
-  it "does not add strike if letter has been guessed" do
-  end
+# method to create num of guesses
+# determined by length of word
+# steps:
+#   length of word is worth 1/2 a guess per char and each unique
+#   character is worth 1.5 a guess
 
-end
+# method to return a message for win or lose
+# steps:
+#   if win "Good job"
+#   else "Go back to school, bro."
