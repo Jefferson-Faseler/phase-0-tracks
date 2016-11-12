dd0c41903d4f618fd18f7f3c814bebb8dde42c97
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-11 22:42:58 -0500

    Add user input on intialization.

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,3 +1,10 @@
 class Hangman
+  def initialize(word)
+    @word = word
+  end
+
+  def word
+    @word
+  end
   
 end
--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -10,27 +10,43 @@
 
 # define methods for:
 # user to enter word
+# steps:
+#   pass in word as argument upon initialization
+# output: return word
+
+# method to create num of guesses
+# determined by length of word
+# steps:
+#   length of word is worth 1/2 a guess per char and each unique
+#   character is worth 1.5 a guess
+# output: guess count to be given at intialization
 
 # transform word into list with values
 # steps: 
 #   split string into hash with letters as keys
 #   set default values as false to indicate not guessed
+# output: return word hash
 
 # another user to enter letters for guess
 # input letter as argument
 # steps: 
 #   compare to guess-hash
+# output: return comparison
 
 # compare guessed letter to guess-hash
 # input instance variable
 # steps: 
 #   compare letter to each letter in guess-hash
 #   if letter is equal to index change value to true
+#     if already true do not update guess count
+#     else update guess count
+# output: updated guess hash values and guess counter
 
 # compare the guessed letter to the word
 # steps: 
 #   compare letter to each letter in word-hash
 #   if letter is equal to index change value to true
+# output: return word hash with updated values
 
 # display a printed version of the 
 # correctly guessed letters with underscores 
@@ -39,14 +55,42 @@
 #   Print hash symbols for all values in word-hash that 
 #   evaluate to true. 
 #   If they evaluate to false print an underscore
-
-# method to create num of guesses
-# determined by length of word
-# steps:
-#   length of word is worth 1/2 a guess per char and each unique
-#   character is worth 1.5 a guess
+# output: print hash symbols of word hash if value is true
 
 # method to return a message for win or lose
 # steps:
 #   if win "Good job"
 #   else "Go back to school, bro."
+# output: printed message
+
+require_relative 'hangman'
+
+describe Hangman do
+let(:hangman) { Hangman.new('abc') }
+
+  it "allows user to enter word" do
+    expect(hangman.word).to eq 'abc'
+  end
+
+  it "creates number of guesses based on length and uniqe chars" do
+  end
+
+  it "transforms string to hash with default values false" do
+  end
+
+  it "user enters letter" do
+  end
+
+  it "compares letter to hash of guessed letters" do
+  end
+
+  it "compares letter to hash of word letters" do
+  end
+
+  it "display word with underscores as unguessed characters" do
+  end
+
+  it "displays win/lose message" do
+  end
+
+end
