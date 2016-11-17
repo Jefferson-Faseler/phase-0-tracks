1c5eb01179c27d55277bf6f7b6b673de80cb1eb2
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-17 12:37:04 -0500

    Add psudeocode and function.

--- js/explore.js
+++ js/explore.js
@@ -1,4 +1,17 @@
 // Take a string as a parameter and then reverse the string
   // for each letter in the string print the letter behind it
-  
-  
+// Steps: Take string into function as argument. 
+//   In the function make the beginning variable the (string.length - 1)
+//   stop iterating when the variable is greater than or equal to 0
+//   increment down by 1
+// output: log each iteration to the screen
+
+var myString = '!sdrawkcab detnirp saw siht';
+
+function reverseIt(string) {
+  for (var i = (string.length -1); i >= 0; i--) {
+    console.log(string[i]);
+  }
+}
+
+reverseIt(myString);
