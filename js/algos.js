da4f02f535ee6b7538836ed6b88cfcdc74637a7d
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-18 10:58:08 -0500

    Add pseudocode for release 0.

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+// function that takes an array of words or phrases and 
+// returns the longest word or phrase. 
+// Steps: create for loop with conditional logic inside
+// ==> for loop compares each element within the array to
+// ==> each other element.
+// output: return longest element
+
+// conditional logic within the for loop
+// steps: If var is less than current element reassign var.
+// ==> Move on to next element.
+// output: return var with stored element
