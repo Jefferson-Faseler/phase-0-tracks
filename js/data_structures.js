f3be5ae5195cb9dfe2cf0b7b7e48b6b1e6c0d459
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-17 20:00:51 -0500

    Add pseudocode for horse creator.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,6 @@
 var colors = ['blue', 'green', 'red', 'yellow'];
 var names = ['Ed', 'Ned', 'Ted', 'Jed'];
+var horses = {};
 
 colors.push('#A93768');
 names.push('Fred');
@@ -7,3 +8,16 @@ names.push('Fred');
 console.log(colors);
 console.log(names);
 
+// Keys are horse names
+// Values are colors
+// Create variable of horse object
+
+// Only create horses with function if the color and name arrays are the same length
+// Steps: Use an if statement to compare the two arrays
+// output: true or false
+
+// Steps: assign horse object
+// ==> use two for loops to add the names as keys and values as colors
+// ==> loop based upon length of array
+// ==> use color for loop inside of name for loop
+// output: horse key/value pairs
