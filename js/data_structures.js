3881d29f4fd2f448b87634dd31dc6986eeface2c
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-17 19:32:23 -0500

    Initial commit. Add arrays.

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ['blue', 'green', 'red', 'yellow'];
+var names = ['Ed', 'Ned', 'Ted', 'Jed'];
+
+colors.push('#A93768');
+names.push('Fred');
+
+console.log(colors);
+console.log(names);
+
