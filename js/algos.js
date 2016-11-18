096586990d79f63f88e8b2d4ac4734c270ad224d
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-18 11:04:55 -0500

    Add loop and conditional logic.

--- js/algos.js
+++ js/algos.js
@@ -9,3 +9,15 @@
 // steps: If var is less than current element reassign var.
 // ==> Move on to next element.
 // output: return var with stored element
+
+var itemsOnMyDesk = ['Mouse', 'Keyboard', 'Clock', 'Computer',
+'Memo Book', 'Candle', 'Mousepad', 'Photo of Family'];
+
+for (var i = 0; i < itemsOnMyDesk.length; i++) {
+  var longest = itemsOnMyDesk[0];
+  if (longest <= itemsOnMyDesk[i]) {
+    longest = itemsOnMyDesk[i];
+  }
+}
+
+console.log(longest);
