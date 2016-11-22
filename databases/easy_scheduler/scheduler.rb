694f17e29a92c6fc1a5b382a0b15bcfdc31dc840
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-22 18:03:58 -0500

    Initial commit.

---
+++ databases/easy_scheduler/scheduler.rb
@@ -0,0 +1,9 @@
+# An application that compares windows of time and openings so that 
+# people can find ways to meet together. 
+# If I have Tuesday morning, wed afternoon and wed night open and that is it 
+# And Steve has Monday morning, Wed night, and Friday morning open 
+# We can cross reference the times with the program 
+# and it will tell us when we have openings
+
+# to get more ambitous if you have more than one time 
+# slot that overlap it will tell you how long of a time slot each is.
