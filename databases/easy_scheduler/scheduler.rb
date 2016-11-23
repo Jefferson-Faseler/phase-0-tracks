d30845d90f7fe519753741c03ad0a5e627262725
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-23 09:06:41 -0500

    Add content and table commands.

--- databases/easy_scheduler/scheduler.rb
+++ databases/easy_scheduler/scheduler.rb
@@ -7,3 +7,59 @@
 
 # to get more ambitous if you have more than one time 
 # slot that overlap it will tell you how long of a time slot each is.
+
+# user enters their name and then a day of the week
+# and any time block they are free
+
+
+
+
+require 'sqlite3'
+require 'faker'
+
+database = SQLite3::Database.new('schedules.db')
+
+# user table command
+create_user_table = <<-SCRIPT
+  CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50)
+  )
+SCRIPT
+
+# time table command
+create_time_table = <<-SCRIPT
+  CREATE TABLE IF NOT EXISTS times(
+    id INTEGER PRIMARY KEY,
+    time VARCAHR(15)
+  )
+SCRIPT
+
+# day table command
+create_day_table = <<-SCRIPT
+  CREATE TABLE IF NOT EXISTS days(
+    id INTEGER PRIMARY KEY,
+    day VARCHAR(15)
+  )
+SCRIPT
+
+# time to schedule joiner
+create_time_schedule_joiner = <<-SCRIPT
+  CREATE TABLE IF NOT EXISTS times_schedules(
+  )
+
+# schedule table command
+create_schedule_table = <<-SCRIPT
+  CREATE TABLE IF NOT EXISTS schedules(
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    opening INTEGER,
+    time INTEGER,
+  )
+SCRIPT
+
+# create table
+database.execute(create_schedule_table)
+
+# test users
+database.execute("INSERT INTO schedules")
