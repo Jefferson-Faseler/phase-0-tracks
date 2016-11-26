83059fd3703a4ad64cd8e4ada641b3aeb760c05b
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-26 12:37:54 -0500

    Add schedule method to user interface.

--- databases/easy_scheduler/scheduler_ui.rb
+++ databases/easy_scheduler/scheduler_ui.rb
@@ -14,4 +14,24 @@ def new_user(database)
   return new_user
 end
 
+def schedule_times
+  confirm = nil
+  until confirm == 'yes' || confirm == 'EXIT'
+    puts "Enter the day you would like to add to your schedule or enter 'EXIT' to quit"
+    puts "(Days are case sensitive and cannot be abbreviated)"
+    day = gets.chomp
+    puts "Now enter the hour in 24 clock format"
+    time = gets.chomp
+    puts "How long will you be free during this block?"
+    length = gets.chomp
+    puts "The day you entered is: #{day}"
+    puts "The time you entered is: #{time}"
+    puts "And you said you would be for: #{length} hours"
+    puts "Is this information correct?"
+    confirm = gets.chomp
+  end
+end
+
+
 new_user(database)
+schedule_times
