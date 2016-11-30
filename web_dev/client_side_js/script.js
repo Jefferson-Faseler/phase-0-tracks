008dabf9adbf5c45714fa1e95b5b8a431de47f79
Jefferson-Faseler <jjfaseler@gmail.com>
2016-11-30 14:05:16 -0500

    Add element to the index page.

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -3,3 +3,8 @@ var arrStrong = document.getElementsByTagName('strong');
 for (var i = 0; i < arrStrong.length; i++) {
   arrStrong[i].style.color = 'blue';
 }
+
+var getList = document.getElementById('elementstring')
+var newListEl = document.createElement('li');
+newListEl.appendChild(document.createTextNode('My goldfish is beautiful'));
+getList.appendChild(newListEl);
